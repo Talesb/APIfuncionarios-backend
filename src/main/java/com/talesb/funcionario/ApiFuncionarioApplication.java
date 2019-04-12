3ebd99e8ba798e84b5e3abf78e5391d6a4186a06
Talesb <talesbatistafig@hotmail.com>
2019-04-11 22:49:12 -0300

    Commit inicial

---
+++ src/main/java/com/talesb/funcionario/ApiFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package com.talesb.funcionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFuncionarioApplication.class, args);
+	}
+
+}
